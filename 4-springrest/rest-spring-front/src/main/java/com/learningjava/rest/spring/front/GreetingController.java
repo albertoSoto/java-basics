66ee5779babeefb56efb5cd5e169c65bec368c82
alberto.soto@gmail.com <deicos2012>
2018-02-08 10:36:02 +0100

    spring rest module valid

---
+++ 4-springrest/rest-spring-front/src/main/java/com/learningjava/rest/spring/front/GreetingController.java
@@ -0,0 +1,23 @@
+package com.learningjava.rest.spring.front;
+
+import com.iesemilidarder.spring.core.Greeting;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+@RequestMapping(path = "/rest/api/v1")
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping(path = "/greeting",method = RequestMethod.GET)
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
+    }
+}
